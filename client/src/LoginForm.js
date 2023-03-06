3dd02163ad4b01753e0690b512e00e8d17709536
Tanner <troxs690@gmail.com>
2023-03-06 16:23:31 -0600

    misc

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,15 +1,30 @@
 import { Grid, Paper, Avatar, TextField,Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+
+
 function LoginForm() {
+const [username, setUsername] = useState("")
+const [password, setPassword] = useState("")
+console.log( username)
+console.log(password)
   const paperStyle = {
     padding: 20,
     height: "70vh",
     width: 280,
     margin: "20px auto",
   };
+
+  function handleLoginSubmit(e) {
+    e.preventDefault()
+    fetch()
+  }
+
+  
   const avatarStyle = { backgroundColor: "red" };
   return (
+    <form onSubmit={handleLoginSubmit} >
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align="center">
@@ -18,14 +33,18 @@ function LoginForm() {
           </Avatar>
           <h2>Sign in</h2>
         </Grid>
-        <TextField id="standard-basic" label="Username" variant="standard" fullWidth />
-        <TextField id="standard-basic" label="Password" variant="standard" fullWidth/>
+        <TextField onChange={e => setUsername(e.target.value)}id="standard-basic" label="Username" variant="standard" fullWidth />
+        <TextField onChange={e => setPassword(e.target.value)}id="standard-basic" label="Password" variant="standard" fullWidth/>
      
       
        <Button id="loginButton" type="submit" color="primary" variant="contained" fullWidth>Sign in</Button>
 
       </Paper>
     </Grid>
+
+    </form>
+
+
   );
 }
 
