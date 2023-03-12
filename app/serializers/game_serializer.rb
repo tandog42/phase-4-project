a87a25f760774941af180cefaf88b3b8b977015c
Tanner <troxs690@gmail.com>
2023-03-12 16:30:03 -0500

    assoc

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,4 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :title, :genre, :platform, :image_url
+ 
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :body
-  has_one :game
-  has_one :user
+
 end
