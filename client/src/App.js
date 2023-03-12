63ace3e207291e9857f4f354916c00812c04d0b0
Tanner <troxs690@gmail.com>
2023-03-12 16:30:19 -0500

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 import ButtonAppBar from "./ButtonAppBar";
 import LoginForm from "./LoginForm";
+import Signup from "./Signup";
 function App() {
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = formName => setCurrentForm(formName);
+
   return (
     <div className="App">
       <ButtonAppBar />
-      <LoginForm />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            currentForm === "login" ? (
+              <LoginForm toggleForm={toggleForm} />
+            ) : (
+              <Signup toggleForm={toggleForm} />
+            )
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,12 +1,10 @@
-import { Grid, Paper, Avatar, TextField, Button } from "@mui/material";
 import React, { useState } from "react";
+import { Grid, Paper, Avatar, TextField, Button } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-
-function LoginForm({ toggleForm }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-
+function Signup({ toggleForm }) {
+  const [username, setName] = useState("");
+  const [password, setPass] = useState("");
+  const [errors, setErrors] = useState([])
   const paperStyle = {
     padding: 20,
     height: "70vh",
@@ -14,59 +12,61 @@ function LoginForm({ toggleForm }) {
     margin: "20px auto",
   };
 
-  function handleLoginSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
+
     const user = {
       username,
-      password,
-    };
-    fetch(`/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
-    }).then(res => {
-      if (res.ok) {
-        res.json().then(user => console.log(user));
-      } else {
-        res.json().then(json => setErrors(Object.entries(json.errors)));
-      }
-    });
-  }
+      password
+    }
+    fetch(`/users`,{
+      method:'POST',
+      headers:{'Content-Type': 'application/json'},
+      body:JSON.stringify(user)
+    })
+    .then(res => {
+        if(res.ok){
+            res.json().then(user => console.log(user))
+        }else {
+            res.json().then(json => setErrors(Object.entries(json.errors)))
+        }
+    })}
+
   const avatarStyle = { backgroundColor: "red" };
   return (
     <>
-      <form onSubmit={handleLoginSubmit}>
+      <form onSubmit={handleSubmit}>
         <Grid>
           <Paper elevation={10} style={paperStyle}>
             <Grid align="center">
               <Avatar style={avatarStyle}>
                 <LockOutlinedIcon />
               </Avatar>
-              <h2>Sign in</h2>
+              <h2>Sign up</h2>
             </Grid>
             <TextField
-              onChange={e => setUsername(e.target.value)}
+              onChange={e => setName(e.target.value)}
               id="standard-basic"
-              label="Username"
+              label="Create a Username..."
               variant="standard"
               fullWidth
             />
             <TextField
-              onChange={e => setPassword(e.target.value)}
+              onChange={e => setPass(e.target.value)}
               id="standard-basic"
-              label="Password"
+              label="Create a Password..."
               variant="standard"
               fullWidth
             />
 
             <Button
-              id="loginButton"
+              id="signupButton"
               type="submit"
               color="primary"
               variant="contained"
               fullWidth
             >
-              Sign in
+              Submit
             </Button>
             <Button
               id="accButton"
@@ -74,17 +74,15 @@ function LoginForm({ toggleForm }) {
               color="primary"
               variant="contained"
               fullWidth
-              onClick={() => toggleForm("register")}
+              onClick={() => toggleForm("login")}
             >
-              Create Account
+              Already have Account?
             </Button>
-            {errors ? <div>{errors}</div> : null}
+      {errors ? errors.map(e => <div>{e[0]+': ' + e[1]}</div>):null}
           </Paper>
         </Grid>
       </form>
-      {/* {errors?errors.map(e => <div>{e[0]+': ' + e[1]}</div>):null} */}
     </>
   );
 }
-
-export default LoginForm;
+export default Signup;
