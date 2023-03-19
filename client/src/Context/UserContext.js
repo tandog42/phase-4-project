55330387f85c441b6f50de3c9aeaf9da4cedf8d9
Tanner <troxs690@gmail.com>
2023-03-19 18:28:17 -0500

    dry

--- client/src/Context/UserContext.js
+++ client/src/Context/UserContext.js
@@ -6,62 +6,49 @@ const UserContext = createContext();
 function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  
-
   const nav = useNavigate();
 
   useEffect(() => {
-    fetch("/me")
-    .then(res => {
+    fetch("/me").then(res => {
       if (res.ok) {
         res.json().then(user => setUser(user));
       }
     });
   }, []);
 
-  function handleLoginSubmit(e) {
+  function handleLoginSubmit(e, loginData) {
     e.preventDefault();
-
-    const user = {
-    username,
-    password,
-    };
 
     fetch(`/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
+      body: JSON.stringify(loginData),
     }).then(res => {
       if (res.ok) {
-        res.json().then(user => setUser(user))
-          .then(nav("/games"))
+        res
+          .json()
+          .then(user => setUser(user))
+          .then(nav("/games"));
       } else {
         res.json().then(err => setErrors(Object.entries(err.error)));
       }
     });
   }
 
-  function handleSubmit(e) {
+  function handleSubmit(e, signupData) {
     e.preventDefault();
-
-    const user = {
-      username,
-      password,
-    };
     fetch(`/users`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
+      body: JSON.stringify(signupData),
     }).then(res => {
       if (res.ok) {
         res
           .json()
-          .then(user => setUser(user))
-          .then(() => nav("/games"));
+          .then(newUser => setUser(newUser))
+          .then(nav("/games"));
       } else {
-        res.json().then(json => setErrors(Object.entries(json.errors)));
+        res.json().then(json => setErrors(Object.entries(json.error)));
       }
     });
   }
@@ -71,16 +58,25 @@ function UserProvider({ children }) {
     }).then(r => {
       if (r.ok) {
         setUser(null);
+      } else {
+        r.json().then(e => alert(e.errors));
       }
     });
   }
-
+  
   return (
     <UserContext.Provider
-      value={{  errors, user, setUser, handleLoginSubmit, handleSubmit, onClickLogout, setUsername, setPassword, password, username }}
+      value={{
+        errors,
+        user,
+        setUser,
+        handleLoginSubmit,
+        handleSubmit,
+        onClickLogout,
+      }}
     >
       {children}
     </UserContext.Provider>
   );
 }
-export { UserContext, UserProvider } 
+export { UserContext, UserProvider };
