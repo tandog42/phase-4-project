80dac05cc08a3a82c552969dbfb9f6583c2a9496
Tanner <troxs690@gmail.com>
2023-03-21 13:27:20 -0500

    d

--- client/src/Game.js
+++ client/src/Game.js
@@ -5,7 +5,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import { Link } from "react-router-dom";
-function Game({ game, }) {
+function Game({ game }) {
 
  
 
