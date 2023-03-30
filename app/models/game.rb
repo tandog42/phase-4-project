09e1d59a2b2fb9840e73e5d29b8a86464276522b
Tanner <troxs690@gmail.com>
2023-03-30 10:30:48 -0500

    added validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  validates :image_url, format: {with: /\.(png|jpg)\Z/i}
+  validates :title, presence: true
+  validates :genre, presence: true
+  validates :platform, presence:true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,11 @@ class Review < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
+  validates :body, presence: true, length: {maximum: 250}
+
+  def game_id
+self.game.id
+  end
 
   def username
     self.user.username
