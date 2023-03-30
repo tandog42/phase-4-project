7a1fba11674f6b7682e964a3c8f3fb42569ef1a7
Tanner <troxs690@gmail.com>
2023-03-30 10:33:20 -0500

    added gameprovider

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./Context/UserContext"
+import { GameProvider } from "./Context/GameContext";
 
 ReactDOM.render(
   <BrowserRouter>
+  <GameProvider>
   <UserProvider>
     <App />
     </UserProvider>
+    </GameProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
