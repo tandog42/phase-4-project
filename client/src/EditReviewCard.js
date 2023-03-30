0d1cebb09cc86dcf0ad7dd700c25512d3d7cea30
Tanner <troxs690@gmail.com>
2023-03-30 10:32:25 -0500

    patch req

--- client/src/EditReviewCard.js
+++ client/src/EditReviewCard.js
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-
-
+import { GameContext } from "./Context/GameContext";
 function EditReviewCard({
- 
- 
-setUpdatedReview,
- onSubmit,
-  review,
   setEditReview,
+
+  review,
+  setReviews,
   
 }) {
+  const { setGames, games } = useContext(GameContext);
+  const [updatedReview, setUpdatedReview] = useState("");
   
 
- 
+  function onSubmit(id) {
+    const data = {
+      body: updatedReview,
+    };
+
+    fetch(`/reviews/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then(r => r.json())
+      .then(r => {
+        const updatedGames = games.map(game => {
+          if (game.id === r.game_id) {
+            const updatedGameReviews = game.reviews.map(review => {
+              if (review.id === r.id) {
+                return r;
+              } else {
+                return review;
+              }
+            });
+            return { ...game, reviews: updatedGameReviews };
+          } else {
+            return game;
+          }
+        });
+      setGames(updatedGames)
+      });
+   
+    setEditReview(null);
+  }
+
   return (
     <div>
-      <Card id="reviewCard">
+      <Card key={review.id} id="reviewCard">
         <CardContent>
           <Typography
             gutterBottom
