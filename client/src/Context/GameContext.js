51ad4d4796c9966378c3b0029135037be99e679e
Tanner <troxs690@gmail.com>
2023-03-30 10:29:32 -0500

    added games context

---
+++ client/src/Context/GameContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useEffect } from "react";
+
+const GameContext = createContext();
+
+function GameProvider({ children }) {
+  const [games, setGames] = useState([]);
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    fetch(`/games`).then(r => {
+      if (r.ok) {
+        r.json().then(allGames => setGames(allGames));
+      } else {
+        r.json().then(e => setErrors(Object.entries(e.errors)));
+      }
+    });
+  }, []);
+  
+  return (
+    <GameContext.Provider
+      value={{
+        games,
+        setGames,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+}
+export {GameContext, GameProvider}
