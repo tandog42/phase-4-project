cf51cc3548c510b76c65319b9c5c39dadf7f1c0c
Tanner <troxs690@gmail.com>
2023-04-07 09:43:56 -0500

    complete

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
-  validates :image_url, format: {with: /\.(png|jpg)\Z/i}
+   validates :image_url, format: {with: /\.(png|jpg)\Z/i}
   validates :title, presence: true
   validates :genre, presence: true
   validates :platform, presence:true
