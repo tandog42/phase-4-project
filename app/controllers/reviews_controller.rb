dbcf9b72c1ad9441b8e8b1ced1a2235bcf903230
Tanner <troxs690@gmail.com>
2023-04-07 09:43:34 -0500

    added status codes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,19 @@
 class ReviewsController < ApplicationController
-  
+  skip_before_action :authorize, only: :index
+
   def index
     render json: Review.all, status: :ok
 end
 
 def show
   review = Review.find(params[:id])
-  render json: review
+  render json: review, status: :ok
 
 end
 
 def create
-review = Review.create(permitted_params)
-render json: review
+review = Review.create!(permitted_params)
+render json: review, status: :created
 end
 
 def update
